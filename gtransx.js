e00f0e9133fc85c8a11aa0536ad94d472181e78f
Leo Perez <leoperezortiz@gmail.com>
2020-12-03 13:04:40 -0600

    Update gtransx.js

--- gtransx.js
+++ gtransx.js
@@ -11,33 +11,51 @@ const process = async (browser, src, tgt, service, texts) => {
   await browser
     .defaultBrowserContext()
     .overridePermissions(urlTo, ["clipboard-write", "clipboard-read"]);
+  const results = [];
+  for (let i = 0; i < texts.length; i++) {
+    const page = await browser.newPage();
+    await page.goto(urlTo + texts[i]);
 
-  return new Promise(async (resolve, reject) => {
-    try {
-      const results = [];
-      await Promise.all(
-        texts.map(async (text) => {
-          const page = await browser.newPage();
-          await page.goto(urlTo + text);
+    let value = null;
 
-          let value = null;
-
-          while (value === null) {
-            await page.waitForSelector(".lmt__translations_as_text__text_btn");
-            let element = await page.$(".lmt__translations_as_text__text_btn");
-            value = await page.evaluate((el) => el.textContent, element);
-          }
-
-          console.log(value);
-          results.push({ src: text, tgt: value });
-          page.close();
-        })
-      );
-      resolve(results);
-    } catch (error) {
-      reject(error);
+    while (!value) {
+      await page.waitForSelector(".lmt__translations_as_text__text_btn");
+      let element = await page.$(".lmt__translations_as_text__text_btn");
+      value = await page.evaluate((el) => el.textContent, element);
     }
-  });
+
+    console.log(value);
+    results.push({ src: texts[i], tgt: value });
+    page.close();
+  }
+  return results;
+
+  // return new Promise(async (resolve, reject) => {
+  //   try {
+  //     const results = [];
+  //     await Promise.all(
+  //       texts.map(async (text) => {
+  //         const page = await browser.newPage();
+  //         await page.goto(urlTo + text);
+
+  //         let value = null;
+
+  //         while (value === null) {
+  //           await page.waitForSelector(".lmt__translations_as_text__text_btn");
+  //           let element = await page.$(".lmt__translations_as_text__text_btn");
+  //           value = await page.evaluate((el) => el.textContent, element);
+  //         }
+
+  //         console.log(value);
+  //         results.push({ src: text, tgt: value });
+  //         page.close();
+  //       })
+  //     );
+  //     resolve(results);
+  //   } catch (error) {
+  //     reject(error);
+  //   }
+  // });
 };
 
 module.exports.translate = async (event) => {
@@ -47,12 +65,12 @@ module.exports.translate = async (event) => {
     browserWSEndpoint: chrome.endpoint,
   });
 
-  const { src, tgt, service, texts } = JSON.parse(event.body);
+  // const { src, tgt, service, texts } = JSON.parse(event.body);
 
-  // const src = "es";
-  // const tgt = "en";
-  // const service = 2;
-  // const texts = ["Hola", "Mundo"];
+  const src = "es";
+  const tgt = "en";
+  const service = 2;
+  const texts = ["Hola", "Mundo"];
 
   let results = await process(browser, src, tgt, service, texts);
   console.log(results);
